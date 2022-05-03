24f738ea1882c7d57d1c628575b8d4d0f63ce7a2
Partithura <partithura@pop-os.localdomain>
2022-05-03 14:53:50 -0300

    ProjetoShop

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -5,3 +5,5 @@ class HomeController {
     });
   }
 }
+
+export default new HomeController();
--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import homeController from '../controllers/HomeController';
 
 const router = new Router();
 
-router.get('/', (req, res) => {
-  res.send('olá');
-});
+router.get('/', homeController.index);
 
 export default router;
